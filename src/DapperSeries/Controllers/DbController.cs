d09137ff79e0aa81fc88cef1cb439a46e0ed31b3
David Paquette <david.paquette@usask.ca>
2018-05-29 19:40:31 -0600

    Add Flight entity, table and sample data generator

--- src/DapperSeries/Controllers/DbController.cs
+++ src/DapperSeries/Controllers/DbController.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using System.IO;
 using System.Data;
+using Dapper;
+using DapperSeries.Entities;
 
 namespace DapperSeries.Controllers
 {
@@ -20,7 +22,7 @@ namespace DapperSeries.Controllers
         {
             _connectionString = configuration.GetConnectionString("DefaultConnection");
         }
-        
+
 
         [HttpPost("init")]
         public async Task<IActionResult> Init()
@@ -28,7 +30,7 @@ namespace DapperSeries.Controllers
             var builder = new SqlConnectionStringBuilder(_connectionString);
             var databaseToCreate = builder.InitialCatalog;
             builder.InitialCatalog = "master";
-            using(var connection = new SqlConnection(builder.ConnectionString))
+            using (var connection = new SqlConnection(builder.ConnectionString))
             {
                 await connection.OpenAsync();
                 var command = connection.CreateCommand();
@@ -37,7 +39,7 @@ namespace DapperSeries.Controllers
                         CREATE DATABASE [{0}]", databaseToCreate);
                 await command.ExecuteNonQueryAsync();
 
-                
+
             }
 
             using (var connection = new SqlConnection(_connectionString))
@@ -51,8 +53,42 @@ namespace DapperSeries.Controllers
                 var createProcsCommand = connection.CreateCommand();
                 createProcsCommand.CommandText = System.IO.File.ReadAllText("./Scripts/CreateStoredProcedures.sql");
                 await createProcsCommand.ExecuteNonQueryAsync();
+
+                //Create a set of future flights based on the scheduled flights
+                var scheduledFlights = await connection.QueryAsync<ScheduledFlight>("SELECT * FROM ScheduledFlight");
+                foreach (var scheduledFlight in scheduledFlights)
+                {
+                    var flights = new List<Flight>();
+                    for (int i = 1; i < 60; i++)
+                    {
+                        var currentDate = DateTime.Today.AddDays(i);
+                        if (IsFlightOnDay(currentDate, scheduledFlight))
+                        {
+                            var flight = new Flight()
+                            {
+                                ScheduledFlightId = scheduledFlight.Id,
+                                Day = currentDate,
+                                ScheduledDeparture = new DateTime(currentDate.Year, currentDate.Month, currentDate.Day, scheduledFlight.DepartureHour, scheduledFlight.DepartureMinute, 0),
+                                ScheduledArrival = new DateTime(currentDate.Year, currentDate.Month, currentDate.Day, scheduledFlight.ArrivalHour, scheduledFlight.ArrivalMinute, 0)
+                            };
+                            flights.Add(flight);
+                        }
+                    }
+                    await connection.ExecuteAsync("INSERT INTO Flight(ScheduledFlightId, Day, ScheduledDeparture, ScheduledArrival) VALUES(@ScheduledFlightId, @Day, @ScheduledDeparture, @ScheduledArrival)", flights);
+                }
             }
             return Ok();
+        }
+
+        private bool IsFlightOnDay(DateTime currentDate, ScheduledFlight scheduledFlight)
+        {
+            return (currentDate.DayOfWeek == DayOfWeek.Sunday && scheduledFlight.IsSundayFlight)
+                || (currentDate.DayOfWeek == DayOfWeek.Monday && scheduledFlight.IsMondayFlight)
+                || (currentDate.DayOfWeek == DayOfWeek.Tuesday && scheduledFlight.IsTuesdayFlight)
+                || (currentDate.DayOfWeek == DayOfWeek.Wednesday && scheduledFlight.IsWednesdayFlight)
+                || (currentDate.DayOfWeek == DayOfWeek.Thursday && scheduledFlight.IsThursdayFlight)
+                || (currentDate.DayOfWeek == DayOfWeek.Friday && scheduledFlight.IsFridayFlight)
+                || (currentDate.DayOfWeek == DayOfWeek.Saturday && scheduledFlight.IsSaturdayFlight);
         }
     }
 }
---
+++ src/DapperSeries/Entities/Flight.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace DapperSeries.Entities 
+{
+    public class Flight 
+    {
+        public int Id {get; set;}
+        public int ScheduledFlightId {get; set;}
+        public ScheduledFlight ScheduledFlight { get; set;}
+        public DateTime Day {get; set;}
+        public DateTime ScheduledDeparture {get; set;}
+        public DateTime ActualDeparture {get; set;}
+        public DateTime ScheduledArrival {get; set;}
+        public DateTime ActualArrival {get; set;}   
+    }
+}
